7569c4ec29e3ac36f423956372b1d5e27c6140e5
JT <jtjobe@gmail.com>
2015-04-04 12:43:02 -0400

    Generate Transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+
+  get 'transactions/show'
+
+  get 'transactions/new'
+
+  get 'transactions/edit'
+
+  get 'transactions/create'
+
+  get 'transactions/update'
+
+  get 'transactions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
