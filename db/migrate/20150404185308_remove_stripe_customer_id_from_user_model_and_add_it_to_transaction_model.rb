1a88e46f75654155a4251b1b8a3f316ac1ab6853
JT <jtjobe@gmail.com>
2015-04-06 09:27:49 -0400

    Add belongs_to association to Transaction model, delete unneccesary migration

--- db/migrate/20150404185308_remove_stripe_customer_id_from_user_model_and_add_it_to_transaction_model.rb
+++
@@ -1,6 +0,0 @@
-class RemoveStripeCustomerIdFromUserModelAndAddItToTransactionModel < ActiveRecord::Migration
-  def change
-    remove_column :users, :stripe_customer_id, :integer
-    add_column :transactions, :stripe_customer_id, :string
-  end
-end
