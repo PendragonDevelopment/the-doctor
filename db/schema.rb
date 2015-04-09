830358af26e943478d534bf2618ed0e291fe837e
JT <jtjobe@gmail.com>
2015-04-09 11:45:14 -0400

    Generate Event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409153146) do
+ActiveRecord::Schema.define(version: 20150409153349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20150409153146) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
 
   create_table "hosts", force: :cascade do |t|
     t.integer  "rating"
@@ -73,6 +82,7 @@ ActiveRecord::Schema.define(version: 20150409153146) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "categories"
   add_foreign_key "hosts", "users"
   add_foreign_key "transactions", "users"
 end
