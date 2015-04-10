21ade94c04fbd2cc12b0b24e3c87f1b557b61034
JT <jtjobe@gmail.com>
2015-04-10 13:36:31 -0400

    Add appointment_id to Transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20150406185102) do
+ActiveRecord::Schema.define(version: 20150410173309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -63,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150406185102) do
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.integer  "appointment_id"
   end
 
   add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
@@ -87,6 +95,7 @@ ActiveRecord::Schema.define(version: 20150406185102) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "categories"
   add_foreign_key "host_events", "events"
   add_foreign_key "host_events", "hosts"
   add_foreign_key "hosts", "users"
