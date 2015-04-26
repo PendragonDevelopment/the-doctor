9f2551c9bd556b3c3a7f971eadfcd261ff2bc971
JT <jtjobe@gmail.com>
2015-04-26 18:20:33 -0400

    I dont know

--- db/migrate/20150416155701_fuck_shit_up.rb
+++ db/migrate/20150416155701_fuck_shit_up.rb
@@ -1,6 +1,6 @@
 class FuckShitUp < ActiveRecord::Migration
   def change
-    execute "DROP TABLE #{:host_events} CASCADE"
-    execute "DROP TABLE #{:events} CASCADE"
+    #execute "DROP TABLE #{:host_events} CASCADE"
+    #execute "DROP TABLE #{:events} CASCADE"
   end
 end
