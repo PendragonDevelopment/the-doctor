a112014c14d89ecd6661726338a81688615d0073
Elizabeth Norton <enorton10@gmail.com>
2015-05-03 16:17:07 -0400

    ratings gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   root to: 'visitors#index'
   devise_for :users
   resources :users, :events, :hosts, :activities, :locations
